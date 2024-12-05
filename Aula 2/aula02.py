69cfd8e5bbb91e777df5c97852d466c533fbbb1a
eferreirarc <estefanyferreiracunha26@gmail.com>
2024-12-05 11:26:46 -0300

    Aprendi sobre variáveis.

---
+++ Aula 2/aula02.py
@@ -0,0 +1,6 @@
+print('oi, meu nome é Estéfany e eu sou linda')
+nome = 'Tetézinha2000' # tipo -> str = string
+idade = 20 # tipo -> int = integer
+altura = 1.79 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = bool)
+print('Seja Bem-Vindo', nome, 'sua idade é', idade, 'anos, você tem', altura, 'em metros. Você é maior de idade?', maior_idade)
